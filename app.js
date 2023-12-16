a4f3a99d4d1b488608206c85b81af1905707eea3
Reshma06 <69632730+Reshma06@users.noreply.github.com>
2023-12-16 12:01:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=> {
+    res.status(404).send('<h2>Page not found</h2>')
+})
+
+app.listen(4000);
